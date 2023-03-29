70cd4eb24c9d5d1270e79d0be53563600a7ff2e4
haukkagu <falk.bruskeland@gmail.com>
2023-03-29 16:00:01 +0200

    Add game project boilerplate

---
+++ src/main/java/abadash/InputManager.java
@@ -0,0 +1,27 @@
+package abadash;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InputManager {
+    private Set<KeyCode> pressedKeys;
+
+    public InputManager() {
+        pressedKeys = new HashSet<>();
+    }
+
+    public boolean isPressed(KeyCode keyCode) {
+        return pressedKeys.contains(keyCode);
+    }
+
+    public void handleKeyPress(KeyEvent keyEvent) {
+        pressedKeys.add(keyEvent.getCode());
+    }
+
+    public void handleKeyRelease(KeyEvent keyEvent) {
+        pressedKeys.remove(keyEvent.getCode());
+    }
+}
